2d263b32a1798aca4c7389a2bdab594b26fef1f7
iman4325 <imantanzeem@hotmail.com>
2018-07-10 20:40:42 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -35,7 +35,6 @@ end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-   STUDENT_NAMES.first
-   STUDENT_NAMES[1]
-   STUDENT_NAMES[2]
+   student_names = ["Adele","Beyonce","Cardi B"]
+   puts student_names
 end
