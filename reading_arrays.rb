63b15551063a21e92bd9bf45daeb7f05ca0b65e5
iman4325 <imantanzeem@hotmail.com>
2018-07-10 20:33:50 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -15,25 +15,25 @@ end
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
-   student_names[4]
+  STUDENT_NAMES[4]
 end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
-   student_names[5]
+   STUDENT_NAMES[5]
 end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
-   student_names.first
+   STUDENT_NAMES.first
 end
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
-   student_names.last
+   STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-   student_names[0,1,2]
+   STUDENT_NAMES[0,1,2]
 end
